432bc4fcc368f8dd952697f35700bdfec6178dea
Forrest Kasler <forrest.kasler@claconnect.com>
2023-08-03 16:43:03 -0400

    Super desperate

--- add_target.js
+++ add_target.js
@@ -16,6 +16,8 @@ if (fs.existsSync('./targets.json')){
 
 var captured_favicon = false;
 var desperate = false;
+var super_desperate = false;
+var favicon_url = '';
 
 (async () => {
   let puppet_options = [
@@ -39,7 +41,7 @@ var desperate = false;
 
   page.on('response', async response => {
     let mime_type = response.headers()['content-type'] 
-    if (mime_type === 'image/x-icon' || mime_type === 'image/vnd.microsoft.icon' || desperate) {
+    if (mime_type === 'image/x-icon' || mime_type === 'image/vnd.microsoft.icon' || (desperate && /image/.test(mime_type)) || super_desperate) {
       captured_favicon = true
       console.log(`Collected Favicon From: ${response.url()}`)
       response.buffer().then(file => {
@@ -59,13 +61,24 @@ var desperate = false;
   console.log(`Target Tab Title: ${title}`)
   if(!captured_favicon){
     console.log(`Failed to Passively Collect Favicon... Attempting to Manually Extract`)
-    let favicon_url = await page.evaluate('const iconElement = document.querySelector("link[rel~=icon]");const href = (iconElement && iconElement.href) || "/favicon.ico";const faviconURL = new URL(href, window.location).toString();faviconURL;');
+    favicon_url = await page.evaluate('const iconElement = document.querySelector("link[rel~=icon]");const href = (iconElement && iconElement.href) || "/favicon.ico";const faviconURL = new URL(href, window.location).toString();faviconURL;');
     desperate = true
     await page.goto(favicon_url, {
       waitUntil: 'networkidle2'
     })
   }
 
+  if(!captured_favicon){
+    console.log(`Failed to Actively Collect Favicon... Attempting to One Last Try`)
+    super_desperate = true
+    await page.goto(favicon_url, {
+      waitUntil: 'networkidle2'
+    })
+  }
+
+  if(!captured_favicon){
+    console.log(`Failed to Actively Collect Favicon... Try Downloading it Yourself and Name it ./favicons/${short_name}.ico`)
+  }
   config[short_name] = {
     login_page: login_page, 
     tab_title: title,
